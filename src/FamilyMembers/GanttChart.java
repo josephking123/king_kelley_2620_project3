f74ed6d4e686fbc0e242a15b110482e2bb2d8c24
Joey <kingjl@merrimack.edu>
2024-04-03 19:53:41 -0400

    GanttChart added code for the gantt chart

---
+++ src/FamilyMembers/GanttChart.java
@@ -0,0 +1,80 @@
+package FamilyMembers;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.IntervalCategoryDataset;
+import org.jfree.data.gantt.Task;
+import org.jfree.data.gantt.TaskSeries;
+import org.jfree.data.gantt.TaskSeriesCollection;
+
+import javax.swing.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class GanttChart {
+
+    public static void createGanttChart(List<FamilyMember> familyMembers) {
+        TaskSeriesCollection dataset = createDataset(familyMembers);
+        JFreeChart chart = createChart(dataset);
+
+        displayChart(chart);
+    }
+
+    private static TaskSeriesCollection createDataset(List<FamilyMember> familyMembers) {
+        TaskSeries series = new TaskSeries("Family Members");
+
+        for (FamilyMember member : familyMembers) {
+            Date birthDate = parseDate(member.getBirthday());
+            Task task;
+            if (member instanceof Living) {
+                task = new Task(member.getName(), birthDate, new Date());
+            } else if (member instanceof Deceased) {
+                Date deathDate = parseDate(((Deceased) member).getInfo());
+                task = new Task(member.getName(), birthDate, deathDate);
+            } else {
+                continue;
+            }
+            series.add(task);
+        }
+
+        TaskSeriesCollection dataset = new TaskSeriesCollection();
+        dataset.add(series);
+
+        return dataset;
+    }
+
+    private static JFreeChart createChart(IntervalCategoryDataset dataset) {
+        return ChartFactory.createGanttChart(
+                "Family Tree Gantt Chart", 
+                "Family Members", 
+                "Timeline", 
+                dataset, 
+                true, 
+                true, 
+                false 
+        );
+    }
+
+    private static void displayChart(JFreeChart chart) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Gantt Chart");
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame.add(new ChartPanel(chart));
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
+
+    private static Date parseDate(String dateString) {
+        try {
+            return new SimpleDateFormat("dd MM yyyy").parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,18 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import javax.swing.SwingUtilities;
 import FamilyMembers.FamilyMember;
+import FamilyMembers.GanttChart;
 
 public class Main {
     
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             try {
-                ArrayList<FamilyMember> people = FileReader.readInputFile("lib\\sampleInput1.txt");
+                ArrayList<FamilyMember> people = FileReader.readInputFile("lib\\sampleInput2.txt");
                 FamilyTreeGUI gui = new FamilyTreeGUI(people);
                 gui.setVisible(true);
+
+                GanttChart.createGanttChart(people);
             } catch (FileNotFoundException ex) {
                 System.err.println("Input file not found!");
             }
